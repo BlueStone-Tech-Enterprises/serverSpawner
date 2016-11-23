5ad414b7b6cd06b16022ceda8cb3ed291fceac14
James Swift <james.rt.swift@gmail.com>
2016-11-23 18:17:10 +0000

    Coding style and fixes

--- index.js
+++ index.js
@@ -13,80 +13,108 @@ const colors = require('colors');
 const srcdsLocation = "garrysmod/srcds_run";
 const _DEBUG = true;
 
+var Messages = [];
+const WorkerEventStdOut = function(data){
+	Messages[this._process.sb_id] = Messages[this._process.sb_id] || "";
+	for (var i=0; i<data.length; i+=1){
+		if (data.charCodeAt(i)===255){
+			this._process.sb_listener.OnMessage(scon.decode(Messages[this._process.sb_id]).result,this._process);
+			Messages[this._process.sb_id] = "";
+		}else{
+			Messages[this._process.sb_id] += data[i];
+		}
+	}
+};
+
 class Server{
-    
-    constructor( name, map, gamemode, playerCount, port, ip, rconPassword ){
-        
-        this.debug( "Creating new instance for server" );
-        
-        this.process = null;
-        this.commandBuffer = [];
-        this.options = []
-        this.events = new EventEmitter();
-        
-        this.createOptions(name, map, gamemode, playerCount, port, ip, rconPassword);
-        this.createProcess();
-        
-    }
-    
-    emit(){
-        this.events.emit(arguments);
-    }
-    
-    debug( output ){
-        if ( _DEBUG ){
-            process.stdout.write('[ServerSpawner:DEBUGR] '.green);
-            console.log( output );
-        }
-    }
+	
+	constructor( name, map, gamemode, playerCount, port, ip, rconPassword ){
+		
+		this.debug( "Creating new instance for server" );
+		
+		this.process = null;
+		this.commandBuffer = [];
+		this.outputCount = 0;
+		this.outputBuffer = [];
+		this.options = []
+		this.events = new EventEmitter();
+		
+		this.createOptions(name, map, gamemode, playerCount, port, ip, rconPassword);
+		this.createProcess();
+		
+	}
+	
+	emit(){
+		this.events.emit(arguments);
+	}
+	
+	debug( output ){
+		if ( _DEBUG ){
+			process.stdout.write('[ServerSpawner:DEBUGR] '.green);
+			console.log( output );
+		}
+	}
 
-    output( output ){
-        if ( _DEBUG ){
-            process.stdout.write('[ServerSpawner:OUTPUT] '.blue);
-            console.log( output );
-        }
-    }
-    
-    getProcess(){
-        return this.process;
-    }
-    
-    createOptions(name, map, gamemode, playerCount, port, ip, rconPassword){
-        
-        this.rconPassword = rconPassword;
-        this.port = port ? port : '27015';
-        
-        this.options.push('-game garrysmod +map gm_construct');
-        //this.options.push('-autoupdate');
-        //this.options.push('-console');
-        //this.options.push('+hostname ' + name ? name : 'NodeJS Server Instance');
-        //this.options.push('+map ' + map ? map : 'gm_construct');
-        //this.options.push('+sv_gamemode ' + gamemode ? gamemode : 'sandbox');
-        //this.options.push('+maxplayers ' + playerCount ? playerCount : '16');
-        //this.options.push('+port ' + port ? port : '27015');
-        //this.options.push('+rcon_password ' + rconPassword);
-        
-        this.debug('Filled options table for SrcDS');
-    }
-    
-    createProcess(){
-        this.process = spawn(srcdsLocation, this.options, {stdio:['inherit', 'pipe', 'pipe']});
-        this.debug('Created process for SrcDS');
-        this.emit('processCreated', this.getProcess());
-        
-        this.process.stdout.on('data', (data) => {
-
-            data.toString('utf8').split('\n').forEach((element)=>{
-                this.output( data.toString('utf8') );
-                this.emit('output', data.toString('utf8'));
-                if ( data.toString('utf8') == "VAC secure mode is activated.\n" ){
-                    this.emit('serverReady');
-                    this.debug('Server is fully functional');
-                }
-            });
-        });
-    }
-    
+	output( output ){
+		process.stdout.write('[ServerSpawner:OUTPUT] '.blue);
+		console.log( output );
+	}
+	
+	output( output ){
+		process.stdout.write('[ServerSpawner:!ERROR] '.red);
+		console.log( output );
+	}
+	
+	getProcess(){
+		return this.process;
+	}
+	
+	createOptions(name, map, gamemode, playerCount, port, ip, rconPassword){
+		
+		var preOptions = [];
+		preOptions.push('-game garrysmod');
+		preOptions.push('+map ' + map ? map : 'gm_construct');
+		//preOptions.push('-autoupdate');
+		preOptions.push('+hostname ' + name ? name : 'NodeJS Server Instance');
+		preOptions.push('+sv_gamemode ' + gamemode ? gamemode : 'sandbox');
+		preOptions.push('+maxplayers ' + playerCount ? playerCount : '16');
+		preOptions.push('+port ' + port ? port : '27015');
+		preOptions.push('+rcon_password ' + rconPassword);
+		
+		this.rconPassword = rconPassword;
+		this.port = port ? port : '27015';
+		
+		this.options = preOptions.join(' ');
+		this.debug('Filled options table for SrcDS');
+	}
+	
+	createProcess(){
+		this.process = spawn(srcdsLocation, this.options, {stdio:['inherit', 'pipe', 'pipe']});
+		this.debug('Created process for SrcDS');
+		this.emit('processCreated', this.getProcess());
+		
+		this.process.stdout.on('data', (data) => {
+			for (var i=0; i<data.length; i+=1){
+				if (data.charCodeAt(i)===12){
+					this.processOutput(this.outputBuffer[this.outputCount]);
+					this.outputCount++;
+					this.outputBuffer[this.outputCount] = "";
+				}else{
+					this.outputBuffer[this.outputCount] += data[i];
+				}
+			}
+		});
+	}
+	
+	processOutput(sOutput){
+		this.output( sOutput );
+		this.emit('output',sOutput);
+		if ( sOutput == "VAC secure mode is activated." ){
+			this.emit('serverReady');
+			this.debug('Server is fully functional');
+		}
+	}
+	
 }
 
 var server = new Server("test", null, null, null, 27015, '0.0.0.0.', 'testing');
