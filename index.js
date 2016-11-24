734a11601bc934bd9d5bc1785a14189e8b91d2a1
James R Swift <james.rt.swift@gmail.com>
2016-11-24 15:54:31 +0000

    Adding loggger to interface with console output

--- index.js
+++ index.js
@@ -6,6 +6,8 @@
  * James R Swift, (c) 2016
 ************************/
 
+"use strict";
+
 const Server = require('./lib/server.class.js');
 
 var server = new Server("test", "gm_construct", "sandbox", 16, 27015, '0.0.0.0', 'testing');
